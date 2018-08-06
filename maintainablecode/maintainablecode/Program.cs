5192ab759d11db650b500f1b78bba5c49b41e982
Laxmi Tiruvuri (Inconsulting Inc.) <v-latiru@microsoft.com>
2018-08-06 12:51:41 -0700

    new changes

--- maintainablecode/maintainablecode/Program.cs
+++ maintainablecode/maintainablecode/Program.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace maintainablecode
 {
@@ -29,6 +31,67 @@ namespace maintainablecode
 
             return this == program;
         }
+
+        /// <summary>
+        /// Inverting Conditionals and Early Returns
+       // Refactor the code below
+
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="valueToMatch"></param>
+        /// <returns></returns>
+
+        public static int GetFirstMatchingIndex(List<int> nums, int valueToMatch)
+        {
+            int matchingIndex = -1;
+
+            if(nums == null)
+            {
+                return matchingIndex;
+            }
+
+            if (nums.Count == 0)
+            {
+                return matchingIndex;
+            }
+
+            
+            for (var i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] == valueToMatch)
+                {
+                    return i;
+                }
+            }
+           
+
+            return matchingIndex;
+        }
+
+        /// <summary>
+        /// Constants
+        //1.	Copy the code below into visual studio: (you will need to add System.Threading to your using statements)
+
+        /// </summary>
+
+        public static void DoStuff()
+        {
+
+            const string welcomChort = "hello Cohort";
+            const int DurationSleep = 2000;
+            Console.WriteLine(welcomChort);
+            Thread.Sleep(DurationSleep);
+            
+
+            var jobTypes = new string[] { "PM", "Dev", "Other" };
+            Console.WriteLine("The job types are:");
+            foreach (var type in jobTypes)
+                Console.WriteLine(type);
+
+            Console.WriteLine("Goodbye Cohort!");
+        }
+
+
 
     }
 }
